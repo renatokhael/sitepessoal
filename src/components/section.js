043bac0fd2956d1a663275371648c12e229a606e
Renato Khael <renatokhael@gmail.com>
2024-08-12 19:41:28 -0300

    feat: adjust section

--- src/components/section.js
+++ src/components/section.js
@@ -1,6 +1,6 @@
 export default function Section({ children }) {
   return (
-    <section className="min-h-[600px] pt-24 bg-gradient-to-r from-[#003339] to-black">
+    <section className="min-h-[600px] pt-24 bg-gradient-to-r from-[#003339] to-black p-10 xl:p-0 ">
       <div className="container mx-auto flex flex-col ">{children}</div>
     </section>
   );
